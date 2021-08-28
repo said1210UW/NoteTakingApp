aa345b938348e38027f46f5d2ba2a22bdf7765e0
said1210 <said1210@uw.edu>
2021-08-27 18:28:10 -0700

    Final Product V1 Will never touch again!

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-//Said Sheck
-//Aug 11th 2021
+/**
+* Said Sheck
+* Aug 11th 2021
+* Note Taking App
+* The following application allows users to enter in notes and delete them as they wish
+*/
+
 "use strict";
 (function() {
   window.addEventListener('load', innit);
 
-  function innit() {
-    //do any dom stuff here
+  function innit() { //do any dom stuff here
     let enterButton = document.getElementById("enter");
-    let entryAmount = document.getElementsByClassName("card");
+    let entryAmount = document.getElementsByClassName("card");//gets all 'card' labeled items
 
     //Enter EventListner
     enterButton.addEventListener('click',function(){
       if (entryAmount.length < 5) { //Limit Cards to Avoid OverFilling
         createCard(returnText(), entryAmount.length + 1);
+        deleteStructure();
       }
     });
 
@@ -21,11 +26,21 @@
 
   }
 
+  /**
+   * The following function allows for the saving of inserted text
+   * @returns enteredText - The string of the inserted text box value
+   */
   function returnText() {
     let enteredText = document.getElementById("textbox").value;
     return enteredText;
   }
 
+  /**
+   * The following function allows for the creation of blank card with its title
+   * and given deleted button
+   * @param {*} enteredText - The string recived from the textbox
+   * @param {*} counter - The interger assocaited with the order the card was created (1st, 2nd, etc)
+   */
   function createCard(enteredText, counter) {
     let card = document.createElement("article");
     let cardContainer = document.getElementById('noteContainer');
@@ -35,6 +50,13 @@
 
   }
 
+  /**
+   * The following method creates the structure for a blank card:
+   * The Title, Content, and Delete Button
+   * @param {*} card - The Unstructed blank card
+   * @param {*} enteredText - The note which would like to be recorded
+   * @param {*} counter - The heading or order the card is associated with
+   */
   function createStructure(card, enteredText, counter) {
     let header = document.createElement("h2");
     header.innerHTML = "Entry " + counter;
@@ -50,6 +72,27 @@
     card.appendChild(clearBtn);
   }
 
+  /**
+   * The following method allows for the removal of a specific entry
+   * The Structure of the code belongs to "StackOverFlow" user "brk"
+   */
+  function deleteStructure(){
+    // add common class to all the buttons
+    let deleteBtn = document.getElementsByClassName("card");
+
+    // converting html collection to array, to use array methods
+    Array.prototype.slice.call(deleteBtn).forEach(function(item) {
+    // iterate and add the event handler to it
+      item.addEventListener("click", function(e) {
+        e.target.parentNode.remove()
+      });
+
+    });
+  }
+
+  /**
+   * The following method allows for the clearing of the textbox
+   */
   function clearText() {
     let textBox = document.getElementById('textbox');
     textBox.value = ""
